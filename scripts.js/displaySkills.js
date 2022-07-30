a1e40827abe61d0e046be37f2c92450e10b00679
paridhishah96 <paridhishah96@gmail.com>
2022-07-30 17:40:58 -0400

    completed everything CSS and added form POSTing method. also started basic js

---
+++ scripts.js/displaySkills.js
@@ -0,0 +1,66 @@
+// const skillsList = [
+//     {
+//         icon: "<span class='devicons devicons-html5'></span>",
+//         description: "HTML5"
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     },
+//     {
+//         icon: ,
+//         description:
+//     }
+// ];
+
+// const populateSkills = function (skillsData) {
+
+// }
