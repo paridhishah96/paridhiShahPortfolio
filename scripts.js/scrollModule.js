91fdab7938e9c69ab6ea4cfa420829d50ba07aa9
paridhishah96 <paridhishah96@gmail.com>
2022-07-31 21:29:37 -0400

    small change

--- scripts.js/scrollModule.js
+++ scripts.js/scrollModule.js
@@ -1,4 +1,1 @@
-{/* <p>I am a Front-End Developer with a passion to build engaging, accessible and responsive apps and websites. Previously, I worked in customer service, where I developed strong problem solving, logical thinking and communication skills.</p>
-
-<p>I carry my love and curiosity for technology into my mindset for coding. My goal is to never stop learning, exploring and growing as a person and a team member.</p> */}
 
