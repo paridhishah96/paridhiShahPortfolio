337cfc04c154968aed1bd6b541b32f96abfd271b
paridhishah96 <paridhishah96@gmail.com>
2022-07-31 21:25:54 -0400

    handled an error for horizontal scroll

--- scripts.js/fadeModule.js
+++ scripts.js/fadeModule.js
@@ -5,7 +5,7 @@ export const faders = document.querySelectorAll(".fadeIn");
 
 const appearOptions = {
     threshold: 1,
-    rootMargin: "0px 0px 70% 0px"
+    rootMargin: "0px 0px 50% 0px"
 };
 export const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
     entries.forEach (entry => {
