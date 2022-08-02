9383fb348b205ad93868a49765fb163b9879c016
paridhishah96 <paridhishah96@gmail.com>
2022-08-02 19:10:21 -0400

    added hamburger menu using JS and some styling

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -2,6 +2,52 @@ import { form, handleSubmit } from "./formModule.js";
 import { faders, appearOnScroll } from "./fadeModule.js";
 
 
+
+
+const navEl = document.querySelector("#slideOutNav");
+const hamIcon = document.querySelector(".navIcon");
+const closeIcon = document.querySelector("#closeButton")
+
+
+hamIcon.addEventListener("click", () => {
+    if (navEl.classList.contains("hideNav") ) {
+        navEl.classList.replace("hideNav", "showNav")
+    } else {
+        return
+    }
+})
+
+closeIcon.addEventListener("click", () => {
+    if (navEl.classList.contains("showNav") ) {
+        navEl.classList.replace("showNav", "hideNav")
+    } else {
+        return
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // init to initialize specific function upon page load
 const init = function () {
     // function to fade in certain elements upon scroll
