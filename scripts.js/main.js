08848fce8921cdf7089bdebe6bb034a7a6958c13
paridhishah96 <paridhishah96@gmail.com>
2022-08-04 16:08:41 -0400

    modularized the skill list

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -2,78 +2,16 @@ import { form, handleSubmit } from "./formModule.js";
 import { faders, appearOnScroll } from "./fadeModule.js";
 import hamburgerMenu from "./hamburgerMenuModule.js";
 import populateSkills from "./displayDataModule.js";
-// import skillsList from "./skillsDataModule";
+import skillsList from "./skillsDataModule.js";
 
 
-const skillsList = [
-    {
-        icon: `<span class="devicons devicons-html5"></span>`,
-        description: "HTML5"
-    },
-    {
-        icon: `<span class="devicons devicons-css3"></span>` ,
-        description: "CSS"
-    },
-    {
-        icon: `<span class="devicons devicons-sass"></span>` ,
-        description: "SASS & SCSS"
-    },
-    {
-        icon: `<span class="devicons devicons-javascript_badge"></span>` ,
-        description: "JavaScript (ES6)"
-    },
-    {
-        icon: `<span class="devicons devicons-jquery"></span>` ,
-        description: "jQuery"
-    },
-    {
-        icon: `<span class="devicons devicons-firebase"></span>` ,
-        description: "Firebase"
-    },
-    {
-        icon: `<i class="devicon-react-original colored"></i>` ,
-        description: "React"
-    },
-    {
-        icon: `<span class="devicons devicons-git"></span>` ,
-        description: "Git"
-    },
-    {
-        icon: `<span class="devicons devicons-github_badge"></span>` ,
-        description: "GitHub"
-    },
-    {
-        icon: `<i class="devicon-vscode-plain colored"></i>` ,
-        description: "VS Code Editor"
-    },
-    {
-        icon: `<span class="devicons devicons-responsive"></span>` ,
-        description: "Responsive"
-    },
-    {
-        icon: `<span class="material-symbols-outlined" aria-hidden="true">group</span>` ,
-        description: "Paired Programming"
-    },
-    {
-        icon: `<span class="material-symbols-outlined" aria-hidden="true">settings_ethernet</span>`,
-        description: "REST APIs"
-    },
-    {
-        icon: `<i class="devicon-python-plain colored"></i>` ,
-        description: "Python"
-    },
-    {
-        icon: `<span class="devicons devicons-terminal"></span>` ,
-        description: "Terminal"
-    }
-];
 
 // init to initialize specific function upon page load
 function init() {
     // opens and closes nav using to click event
     hamburgerMenu();
 
-    // populate skills section
+    // populate skills section with icons and their names using the skill list module
     populateSkills(skillsList);
 
 
